ddb7b7911adbe02e2999f4de5c8ff3c0e085eeb2
Sedat Turkoglu <sedat.turkoglu@sogeti.lu>
2020-07-15 18:47:38 +0200

    last commit from sogeti

--- src/com/sedat/testing/UnitTesting.java
+++ src/com/sedat/testing/UnitTesting.java
@@ -1,5 +1,13 @@
 package com.sedat.testing;
 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.Line;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Mixer;
+import javax.sound.sampled.TargetDataLine;
+
 /**
  * Unit testing usually refers to testing a method.
  * Instead of running unit tests manually, developers can use a unit testing framework to run the tests in an automated fashion.
@@ -10,7 +18,31 @@ package com.sedat.testing;
  */
 public class UnitTesting {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws LineUnavailableException {	
+		
+		AudioFormat format = new AudioFormat(8000.0f,16,1,true,true);
+		TargetDataLine microphone = AudioSystem.getTargetDataLine(format);
+		//microphone.
+		//DataLine.Info info = new DataLine.Info(TargetDataLine.class,format);
+		
+		Mixer.Info[] mixerInfos = AudioSystem.getMixerInfo();
+		 for (Mixer.Info info: mixerInfos){
+		  Mixer m = AudioSystem.getMixer(info);
+		  Line.Info[] lineInfos = m.getSourceLineInfo();
+		  for (Line.Info lineInfo:lineInfos){
+		   System.out.println (info.getName()+"###"+lineInfo);
+		   Line line = m.getLine(lineInfo);
+		   System.out.println("\t#####"+line);
+		  }
+		  lineInfos = m.getTargetLineInfo();
+		  for (Line.Info lineInfo:lineInfos){
+		   System.out.println (m+"---"+lineInfo);
+		   Line line = m.getLine(lineInfo);
+		   System.out.println("\t-----"+line);
+
+		  }
+
+		 }
 		
 	}
 
